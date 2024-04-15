782f023bc8f09501a64525f8ce6c7a887b473e68
popfishy <1301350150@qq.com>
2024-04-15 16:38:05 +0800

    无修改

--- voice2text/iat_ws_python3.py
+++ voice2text/iat_ws_python3.py
@@ -51,11 +51,17 @@ class Ws_Param(object):
         # 公共参数(common)
         self.CommonArgs = {"app_id": self.APPID}
         # 业务参数(business)，更多个性化参数可在官网查看
-        self.BusinessArgs = {"domain": "iat", "language": "zh_cn", "accent": "mandarin", "vinfo":1,"vad_eos":5000}
+        self.BusinessArgs = {
+            "domain": "iat",
+            "language": "zh_cn",
+            "accent": "mandarin",
+            "vinfo": 1,
+            "vad_eos": 5000,
+        }
 
     # 生成url
     def create_url(self):
-        url = 'wss://ws-api.xfyun.cn/v2/iat'
+        url = "wss://ws-api.xfyun.cn/v2/iat"
         # 生成RFC1123格式的时间戳
         now = datetime.now()
         date = format_date_time(mktime(now.timetuple()))
@@ -65,21 +71,24 @@ class Ws_Param(object):
         signature_origin += "date: " + date + "\n"
         signature_origin += "GET " + "/v2/iat " + "HTTP/1.1"
         # 进行hmac-sha256进行加密
-        signature_sha = hmac.new(self.APISecret.encode('utf-8'), signature_origin.encode('utf-8'),
-                                 digestmod=hashlib.sha256).digest()
-        signature_sha = base64.b64encode(signature_sha).decode(encoding='utf-8')
+        signature_sha = hmac.new(
+            self.APISecret.encode("utf-8"),
+            signature_origin.encode("utf-8"),
+            digestmod=hashlib.sha256,
+        ).digest()
+        signature_sha = base64.b64encode(signature_sha).decode(encoding="utf-8")
 
-        authorization_origin = "api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"" % (
-            self.APIKey, "hmac-sha256", "host date request-line", signature_sha)
-        authorization = base64.b64encode(authorization_origin.encode('utf-8')).decode(encoding='utf-8')
+        authorization_origin = (
+            'api_key="%s", algorithm="%s", headers="%s", signature="%s"'
+            % (self.APIKey, "hmac-sha256", "host date request-line", signature_sha)
+        )
+        authorization = base64.b64encode(authorization_origin.encode("utf-8")).decode(
+            encoding="utf-8"
+        )
         # 将请求的鉴权参数组合为字典
-        v = {
-            "authorization": authorization,
-            "date": date,
-            "host": "ws-api.xfyun.cn"
-        }
+        v = {"authorization": authorization, "date": date, "host": "ws-api.xfyun.cn"}
         # 拼接鉴权参数，生成url
-        url = url + '?' + urlencode(v)
+        url = url + "?" + urlencode(v)
         # print("date: ",date)
         # print("v: ",v)
         # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
@@ -103,10 +112,12 @@ def on_message(ws, message):
             for i in data:
                 for w in i["cw"]:
                     result += w["w"]
-            print("sid:%s call success!,data is:%s" % (sid, json.dumps(data, ensure_ascii=False)))
+            print(
+                "sid:%s call success!,data is:%s"
+                % (sid, json.dumps(data, ensure_ascii=False))
+            )
     except Exception as e:
         print("receive msg,but parse exception:", e)
-
 
 
 # 收到websocket错误的处理
@@ -115,7 +126,7 @@ def on_error(ws, error):
 
 
 # 收到websocket关闭的处理
-def on_close(ws,a,b):
+def on_close(ws, a, b):
     print("### closed ###")
 
 
@@ -124,7 +135,9 @@ def on_open(ws):
     def run(*args):
         frameSize = 8000  # 每一帧的音频大小
         intervel = 0.04  # 发送音频间隔(单位:s)
-        status = STATUS_FIRST_FRAME  # 音频的状态信息，标识音频是第一帧，还是中间帧、最后一帧
+        status = (
+            STATUS_FIRST_FRAME  # 音频的状态信息，标识音频是第一帧，还是中间帧、最后一帧
+        )
 
         with open(wsParam.AudioFile, "rb") as fp:
             while True:
@@ -137,25 +150,40 @@ def on_open(ws):
                 # appid 必须带上，只需第一帧发送
                 if status == STATUS_FIRST_FRAME:
 
-                    d = {"common": wsParam.CommonArgs,
-                         "business": wsParam.BusinessArgs,
-                         "data": {"status": 0, "format": "audio/L16;rate=16000",
-                                  "audio": str(base64.b64encode(buf), 'utf-8'),
-                                  "encoding": "raw"}}
+                    d = {
+                        "common": wsParam.CommonArgs,
+                        "business": wsParam.BusinessArgs,
+                        "data": {
+                            "status": 0,
+                            "format": "audio/L16;rate=16000",
+                            "audio": str(base64.b64encode(buf), "utf-8"),
+                            "encoding": "raw",
+                        },
+                    }
                     d = json.dumps(d)
                     ws.send(d)
                     status = STATUS_CONTINUE_FRAME
                 # 中间帧处理
                 elif status == STATUS_CONTINUE_FRAME:
-                    d = {"data": {"status": 1, "format": "audio/L16;rate=16000",
-                                  "audio": str(base64.b64encode(buf), 'utf-8'),
-                                  "encoding": "raw"}}
+                    d = {
+                        "data": {
+                            "status": 1,
+                            "format": "audio/L16;rate=16000",
+                            "audio": str(base64.b64encode(buf), "utf-8"),
+                            "encoding": "raw",
+                        }
+                    }
                     ws.send(json.dumps(d))
                 # 最后一帧处理
                 elif status == STATUS_LAST_FRAME:
-                    d = {"data": {"status": 2, "format": "audio/L16;rate=16000",
-                                  "audio": str(base64.b64encode(buf), 'utf-8'),
-                                  "encoding": "raw"}}
+                    d = {
+                        "data": {
+                            "status": 2,
+                            "format": "audio/L16;rate=16000",
+                            "audio": str(base64.b64encode(buf), "utf-8"),
+                            "encoding": "raw",
+                        }
+                    }
                     ws.send(json.dumps(d))
                     time.sleep(1)
                     break
@@ -169,13 +197,18 @@ def on_open(ws):
 if __name__ == "__main__":
     # 测试时候在此处正确填写相关信息即可运行
     time1 = datetime.now()
-    wsParam = Ws_Param(APPID='83db5cc8', APISecret='MTdhMDJkMjgxMGM3ZGQ2NTViNTMzNmU0',
-                       APIKey='fc2fbe5181a76cc0b4c412bb97e5c36d',
-                       AudioFile=r'/home/yjq/rtasr_python3_demo/python/test_1.pcm')
+    wsParam = Ws_Param(
+        APPID="83db5cc8",
+        APISecret="MTdhMDJkMjgxMGM3ZGQ2NTViNTMzNmU0",
+        APIKey="fc2fbe5181a76cc0b4c412bb97e5c36d",
+        AudioFile=r"/home/yjq/rtasr_python3_demo/python/test_1.pcm",
+    )
     websocket.enableTrace(False)
     wsUrl = wsParam.create_url()
-    ws = websocket.WebSocketApp(wsUrl, on_message=on_message, on_error=on_error, on_close=on_close)
+    ws = websocket.WebSocketApp(
+        wsUrl, on_message=on_message, on_error=on_error, on_close=on_close
+    )
     ws.on_open = on_open
     ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
     time2 = datetime.now()
-    print(time2-time1)
+    print(time2 - time1)
